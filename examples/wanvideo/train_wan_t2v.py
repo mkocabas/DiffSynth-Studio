4316a79faa28a21649436245946620964feaae8a
mervekocabas <merve.kocabas@outlook.com>
2025-05-18 19:26:38 +0200

    uv video training integration

--- examples/wanvideo/train_wan_t2v.py
+++ examples/wanvideo/train_wan_t2v.py
@@ -20,7 +20,7 @@ class TextVideoDataset(torch.utils.data.Dataset):
         self.uv_path = []
         for file_name in metadata["file_name"]:
             base_name = os.path.splitext(file_name)[0]  # Remove extension
-            uv_file = f"{base_name}_uv.mp4"  # Add _uv.mp4
+            uv_file = f"{base_name}.mp4"  
             self.uv_path.append(os.path.join(base_path, "train_uv", uv_file))
 
         self.text = metadata["text"].to_list()
